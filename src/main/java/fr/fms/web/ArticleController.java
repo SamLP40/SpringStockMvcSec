dd5185be0f56c3aac350dd2142760970aefb5bd1
SamSam <sam40230@gmail.com>
2023-05-24 10:39:17 +0200

    Added searchbar+delete request

--- src/main/java/fr/fms/web/ArticleController.java
+++ src/main/java/fr/fms/web/ArticleController.java
@@ -19,15 +19,27 @@ public class ArticleController {
 	
 	//@RequestMapping(value="/index", method=RequestMethod.GET)
 	@GetMapping("/index")
-	public String index(Model model, @RequestParam(name="page", defaultValue="0") int page) { // Paramétrage de la pagination (page initiale = 0, indicateur de page = page)
-		Page<Article> articles= articleRepository.findAll(PageRequest.of(page, 5)); // Récup de toutes les pages
+	public String index(Model model, @RequestParam(name="page", defaultValue="0") int page,
+									 @RequestParam(name="keyword", defaultValue="") String kw) { // Paramétrage de la pagination (page initiale = 0, indicateur de page = page)
+		Page<Article> articles= articleRepository.findByDescriptionContains(kw, PageRequest.of(page, 5));
+	//	Page<Article> articles1= articleRepository.findAll(PageRequest.of(page, 5)); // Récup de toutes les pages
 		model.addAttribute("listArticle", articles.getContent()); // Accès au contenu de chaque page en base
 		
 		model.addAttribute("pages", new int[articles.getTotalPages()]); // Méthode qui permet d'accéder au nb total de pages
 		
 		model.addAttribute("currentPage", page); // Permet d'atteindre la page en cours.
 		
+		model.addAttribute("keyword", kw); // Permet d'enregistrer un mot clé
+		
 		return "articles"; // Renvoie au dispatcheur le fichier articles.html
+	}
+	
+	@GetMapping("/delete") // Requête de suppression (que le programme recherche avec la clé primaire mappée)
+	public String delete(Long id, int page, String keyword) {
+		
+		articleRepository.deleteById(id);
+		
+		return "redirect:/index?page="+page+"&keyword="+keyword;
 	}
 }
 
